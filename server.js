b521832ea206ae865011a668164c1458d587c895
reinaldoriant <reinaldo.perdana@admo.astra.co.id>
2026-02-04 17:59:42 +0700

    feat: add GET /product endpoint

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-import "dotenv/config";
 import express from "express";
+import env from "dotenv";
 
+import { createClient } from "@supabase/supabase-js";
+
+env.config();
+
+// Supabase
+const supabase = createClient(process.env.DATABASE_URL, process.env.DATABASE_KEY);
+
+// Services 
 const app = express();
 
 app.get("/", (_, response) =>
@@ -13,3 +21,14 @@ app.listen(process.env.PORT, () =>
       `: Server is running on port ${process.env.PORT}...`
   )
 );
+
+// Get Product
+app.get("/product", async (_, response) => {
+  try {
+    const { data, error } = await supabase.from("product").select();
+    console.log(data);
+    return response.send(data);
+  } catch (error) {
+    return response.send({ error });
+  }
+});
