66e7cd0098e06037a92602d698042f2c771510da
reinaldoriant <reinaldo.perdana@admo.astra.co.id>
2026-02-04 18:09:06 +0700

    feat: add POST /product endpoint

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const supabase = createClient(process.env.DATABASE_URL, process.env.DATABASE_KEY
 
 // Services 
 const app = express();
+app.use(express.json());
 
 app.get("/", (_, response) =>
   response.json({ info: "Express app with Supabase" })
@@ -30,5 +31,19 @@ app.get("/product", async (_, response) => {
     return response.send(data);
   } catch (error) {
     return response.send({ error });
+  }
+});
+
+// Post Product
+app.post("/product", async (request, response) => {
+  try {
+    console.log(request.body);
+    const { data, error } = await supabase.from("product").insert(request.body);
+    if (error) {
+      return response.status(400).json(error);
+    }
+    response.status(200).json(request.body);
+  } catch (error) {
+    response.send({ error });
   }
 });
